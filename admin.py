1d6c6fa3865125c42a8b3be8c2466260e1af967a
Paweł Taborowski <peantab@gmail.com>
2018-09-09 17:10:36 +0200

    Pierwszy commit: logowanie do Facebooka, listowanie katalogu

---
+++ admin.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from src.directory_lister import DirectoryLister
+from flask import render_template
+from flask import request
+
+app = Flask("src")
+
+
+@app.route("/", methods=['GET'])
+def file_select():
+    directory_lister = DirectoryLister("input")
+    return render_template("fileSelection.html", folder_content=directory_lister.list_folder())
+
+@app.route("/logged", methods=['GET'])
+def logged_in():
+    return render_template("layout.html")
+
+@app.route("/login", methods=['GET'])
+def login():
+    return render_template("facebookLogin.html")
+
+if __name__ == '__main__':
+    app.run(ssl_context='adhoc')
---
+++ src/directory_lister.py
@@ -0,0 +1,18 @@
+import re
+from os import listdir
+
+class DirectoryLister:
+    def __init__(self, directory):
+        self.directory = directory
+        self.extension = re.compile("\..*")
+        self.underscore = re.compile("_")
+
+    def list_folder(self):
+        files = listdir(self.directory)
+        files_formatted = {self._format_filename(original):original for original in files}
+        return files_formatted
+
+    def _format_filename(self, filename):
+        formatted = self.extension.sub(repl="", string=filename)
+        formatted = self.underscore.sub(repl=" ", string=formatted)
+        return formatted
